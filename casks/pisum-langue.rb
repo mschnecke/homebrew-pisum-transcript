850e90629964fca6ad72524c87ad417a61ffd17c
mschnecke <mschnecke@users.noreply.github.com>
2026-03-14 18:01:49 +0000

    Update Pisum Langue cask to v0.1.11

--- casks/pisum-langue.rb
+++ casks/pisum-langue.rb
@@ -1,6 +1,6 @@
 cask "pisum-langue" do
-  version "0.1.7"
-  sha256 "REPLACE_WITH_ACTUAL_CHECKSUM"
+  version "0.1.11"
+  sha256 "9623ef2589cd946b729275c49252117b35fc4b9dfb13eb19800035b0f68884e2"
 
   url "https://github.com/mschnecke/langue/releases/download/v#{version}/Pisum.Langue_#{version}_aarch64.pkg"
   name "Pisum Langue"
